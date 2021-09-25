c3f0c31de495a272dc537cbdd70be2f6b53d14f5
trung-pham-dinh <63339860+trung-pham-dinh@users.noreply.github.com>
2021-09-25 11:39:44 +0700

    replace potentialmeter with sht31 module, send humidity and temperature

--- arduino/nrf-iot/sensor_node/sensor_node.ino
+++ arduino/nrf-iot/sensor_node/sensor_node.ino
@@ -1,10 +1,21 @@
 #include <SPI.h> 
 #include <nRF24L01.h> 
 #include <RF24.h>
+#include<Wire.h>
 
 #define LED 2
 #define PUMP 3
-#define TEMP 0
+
+#define MEASUREMENT 0x2400
+const int SHT31ADDR = 0x44;
+struct HumidTemp {
+  uint16_t raw_temp;
+  uint16_t raw_humid;
+  float temp;
+  float humid;
+};
+HumidTemp humid_temp;
+
 
 long long preTime = millis();
 
@@ -28,13 +39,15 @@ void setup()
   digitalWrite(PUMP, LOW);
   digitalWrite(LED, LOW);
   
-  //Serial.begin(9600); // for receiver
+  Serial.begin(115200); // for receiver
   
   myRadio.begin(); 
   myRadio.openWritingPipe(addresses[0]); // for transmit
   myRadio.openReadingPipe(1, addresses[1]); // for receive
   
   myRadio.setPALevel(RF24_PA_MIN);
+
+  Wire.begin();
   
 }
 
@@ -64,14 +77,17 @@ void loop()
   {
     myRadio.stopListening();
     preTime = millis();
+
+    sendCMD(MEASUREMENT);
+    readData(humid_temp);
     
-    int tempVal = analogRead(TEMP);
-    tempVal = map(tempVal, 0, 1023, 20,40);
-    String string = "!0:iot-temp:";
-    string += String(tempVal) + "#";
-    
-    const char *c_mess = string.c_str();
-    myRadio.write(c_mess, string.length());
+    String tempStr = "!0:iot-temp:" + String(int(humid_temp.temp)) + "#";
+    String humidStr = "!0:iot-humid:" + String(int(humid_temp.humid)) + "#";
+    //Serial.println(humid_temp.humid);
+    const char *c_tempMess = tempStr.c_str();
+    const char *c_humidMess = humidStr.c_str();
+    myRadio.write(c_tempMess, tempStr.length());
+    myRadio.write(c_humidMess, humidStr.length());
   }
 }
 
@@ -92,4 +108,34 @@ Package extract(String mess)
   package.value = value.toInt();
 
   return package;
+}
+
+
+
+
+
+// sht31
+void readByte(uint8_t *data, int numberOfBytes) {
+  Wire.requestFrom(SHT31ADDR, numberOfBytes);
+  for(int i = 0; i < numberOfBytes; i++) {
+    data[i] = Wire.read();
+  }
+}
+
+void sendCMD(uint16_t cmd) {
+  Wire.beginTransmission(SHT31ADDR);
+  Wire.write(cmd >> 8); // send MSB of command
+  Wire.write(cmd & 0x00FF); // send LSB of command
+  Wire.endTransmission();
+}
+
+void readData(HumidTemp &data) {
+  uint8_t bytes[6]; // 2 bytes TEMP, 1 byte CRC, 2 bytes HUMID, 1 bytes CRC
+  readByte(bytes, 6);
+  
+  data.raw_temp = (bytes[0] << 8) | bytes[1];
+  data.raw_humid = (bytes[3] << 8) | bytes[4];
+
+  data.temp = (175.0 / 65535) * data.raw_temp - 45;
+  data.humid = (100.0 / 65535) * data.raw_humid;
 }
